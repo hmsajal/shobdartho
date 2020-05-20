3c85c68e1aebde217e99f78703faf3f7d0ecde28
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-05-21 05:23:31 +0600

    initiated

---
+++ precache-manifest.e13ba67ab3775f0500251bbb23af414e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aaef239b96ba5457f3ac7b2a1d8fb7",
+    "url": "/shobdartho/index.html"
+  },
+  {
+    "revision": "326d0040607691ac86e7",
+    "url": "/shobdartho/static/css/main.181b1247.chunk.css"
+  },
+  {
+    "revision": "4c897b91d5d8ea0c7e20",
+    "url": "/shobdartho/static/js/2.13f01d37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shobdartho/static/js/2.13f01d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326d0040607691ac86e7",
+    "url": "/shobdartho/static/js/main.be60c5e7.chunk.js"
+  },
+  {
+    "revision": "d4d2a8133cd7370e7a1d",
+    "url": "/shobdartho/static/js/runtime-main.a9b75e39.js"
+  }
+]);
